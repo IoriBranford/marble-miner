ef2a30460c19de8407f84de7e6f89158ed0f3e8f
IoriBranford <ioribranford@gmail.com>
2017-01-19 16:01:08 -0800

    The beginning, with menus and music.

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer _instance;
+
+	// Use this for initialization
+	void Start () {
+		if (_instance == null) {
+			GameObject.DontDestroyOnLoad(gameObject);
+			_instance = this;
+		} else {
+			Destroy(gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
