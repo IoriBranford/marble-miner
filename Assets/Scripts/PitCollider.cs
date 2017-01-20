1a97bd27523d3b7bd56aaba2d123d85b98470d84
IoriBranford <ioribranford@gmail.com>
2017-01-20 12:15:08 -0800

    Ball object. End game when it falls off the bottom.

---
+++ Assets/Scripts/PitCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+
+	void OnTriggerEnter2D (Collider2D otherCollider) {
+		print("Trigger");
+		levelManager.LoadLevel("Win");
+	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		print("Collision");
+	}
+}
