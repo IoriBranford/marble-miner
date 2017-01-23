05c22617be255cca3cb139b464f214a9e99bed99
IoriBranford <ioribranford@gmail.com>
2017-01-23 12:55:11 -0800

    At start, hold the ball and launch it with left mouse button.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	public float launchForce;
+	public Paddle paddle;
+
+	private Vector3 _fromPaddle;
+	private bool _started = false;
+
+	// Use this for initialization
+	void Start () {
+		_fromPaddle = transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!_started) {
+			transform.position = paddle.transform.position
+				+ _fromPaddle;
+
+			if (Input.GetMouseButtonDown(0)) {
+				var body = GetComponent<Rigidbody2D>();
+				body.AddForce(new Vector2(0f, launchForce),
+						ForceMode2D.Impulse);
+				_started = true;
+			}
+		}
+	}
+}
