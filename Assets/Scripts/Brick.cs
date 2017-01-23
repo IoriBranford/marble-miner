723476f19870dffc2540c2c319e19610b05bbf77
IoriBranford <ioribranford@gmail.com>
2017-01-23 13:34:54 -0800

    Some bricks.

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+	public int maxHealth;
+	private int _health;
+
+	// Use this for initialization
+	void Start () {
+		_health = maxHealth;
+	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		--_health;
+		print(_health);
+	}
+}
